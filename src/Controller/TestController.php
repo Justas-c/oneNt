91d119e9207194cb78b3d62c7b8bfa4c81b257d2
Justinas Cenys <j.cenys@gmail.com>
2019-11-25 21:40:47 +0200

    added apache pack

---
+++ src/Controller/TestController.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Psr\Log\LoggerInterface;
+
+
+class TestController extends AbstractController
+{
+    // route.yaml - /helloworld
+    public function helloWorld() {
+        echo 'hello world<br>';
+        return new Response('response text');
+    }
+
+    // testing annotations
+    /**
+     * @Route("/")
+     */
+    public function testhome() {
+        echo 'hello home<br>';
+        return new Response('home response text');
+    }
+
+    // test twig
+    /**
+     * @Route("/twigtest")
+     */
+    public function testtwig() {
+        echo 'hello twig<br>';
+        return $this->render('testtmp/testtwig.html.twig');
+    }
+
+    // test twig2
+    /**
+     * @Route("/twigtest2")
+     */
+
+     public function testtwig2() {
+        echo 'test twig2<br>';
+        return $this->render('homepage.html.twig');
+     }
+}
