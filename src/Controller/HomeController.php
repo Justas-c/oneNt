050c5d92b3b979c3b351c519e5a7ccb5991ed4b1
Justinas Cenys <j.cenys@gmail.com>
2019-12-06 18:43:39 +0200

    Reinstalled chrome, making home controller and view, in progress.

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+// for testing
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Home controller
+ */
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+     public function homepage()
+     {
+         return $this->render('home_tmpl/homepage.html.twig');
+     }
+
+     /**
+      * @Route("t1", name="test1")
+      */
+     public function test1()
+     {
+         return new Response('Response stexttt');
+     }
+}
