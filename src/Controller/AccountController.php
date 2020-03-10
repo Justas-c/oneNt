74ada464e9d4f77ee4424b7ab434ce8a9f7b51f1
Justinas Cenys <j.cenys@gmail.com>
2020-03-10 15:23:57 +0200

    added footer to main page, few other minor changes

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -8,15 +8,22 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * AccountController(Login, Register, etc)
+ * @IsGranted("ROLE_USER")
  */
 class AccountController // extends AnotherClass
 {
     /**
      * @Route("/account", name="app_account")
-     * @IsGranted("ROLE_USER")
      */
     public function account()
     {
         die('die(account method)');
     }
+
+    // /**
+    //  * @Route("/add_property", name="app_add_property")
+    //  */
+    // public function addPropertyPost(){
+    //     return $this->render('add_property_form.html.twig');
+    // }
 }
