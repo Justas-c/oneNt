9f7d71f95c6c7ae02bf35e43b624c3ef58de8294
Justinas Cenys <j.cenys@gmail.com>
2020-03-10 15:38:20 +0200

    created property entity and used migrations to create database table

---
+++ src/Entity/Property.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PropertyRepository")
+ */
+class Property
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $gatve;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $plotas;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $kambariuSkaicius;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $miestas;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $butonr;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $aukstas;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $irengimas;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $plotes;
+
+    /**
+     * @ORM\Column(type="array", nullable=true)
+     */
+    private $nuotraukos = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getGatve(): ?string
+    {
+        return $this->gatve;
+    }
+
+    public function setGatve(?string $gatve): self
+    {
+        $this->gatve = $gatve;
+
+        return $this;
+    }
+
+    public function getPlotas(): ?int
+    {
+        return $this->plotas;
+    }
+
+    public function setPlotas(?int $plotas): self
+    {
+        $this->plotas = $plotas;
+
+        return $this;
+    }
+
+    public function getKambariuSkaicius(): ?int
+    {
+        return $this->kambariuSkaicius;
+    }
+
+    public function setKambariuSkaicius(?int $kambariuSkaicius): self
+    {
+        $this->kambariuSkaicius = $kambariuSkaicius;
+
+        return $this;
+    }
+
+    public function getMiestas(): ?string
+    {
+        return $this->miestas;
+    }
+
+    public function setMiestas(?string $miestas): self
+    {
+        $this->miestas = $miestas;
+
+        return $this;
+    }
+
+    public function getButonr(): ?string
+    {
+        return $this->butonr;
+    }
+
+    public function setButonr(?string $butonr): self
+    {
+        $this->butonr = $butonr;
+
+        return $this;
+    }
+
+    public function getAukstas(): ?int
+    {
+        return $this->aukstas;
+    }
+
+    public function setAukstas(?int $aukstas): self
+    {
+        $this->aukstas = $aukstas;
+
+        return $this;
+    }
+
+    public function getIrengimas(): ?string
+    {
+        return $this->irengimas;
+    }
+
+    public function setIrengimas(?string $irengimas): self
+    {
+        $this->irengimas = $irengimas;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getPlotes(): ?string
+    {
+        return $this->plotes;
+    }
+
+    public function setPlotes(?string $plotes): self
+    {
+        $this->plotes = $plotes;
+
+        return $this;
+    }
+
+    public function getNuotraukos(): ?array
+    {
+        return $this->nuotraukos;
+    }
+
+    public function setNuotraukos(?array $nuotraukos): self
+    {
+        $this->nuotraukos = $nuotraukos;
+
+        return $this;
+    }
+}
---
+++ src/Migrations/Version20200310133608.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200310133608 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE property (id INT AUTO_INCREMENT NOT NULL, gatve VARCHAR(255) DEFAULT NULL, plotas INT DEFAULT NULL, kambariu_skaicius INT DEFAULT NULL, miestas VARCHAR(255) DEFAULT NULL, butonr VARCHAR(255) DEFAULT NULL, aukstas INT DEFAULT NULL, irengimas VARCHAR(255) DEFAULT NULL, content LONGTEXT DEFAULT NULL, plotes VARCHAR(255) DEFAULT NULL, nuotraukos LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE property');
+        $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_bin`');
+    }
+}
